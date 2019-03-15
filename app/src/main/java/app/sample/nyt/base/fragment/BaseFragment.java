1307e3419c5ebc9a55c952f5132e93a5ad5a31c8
Aly Naguib <aly.naguib.dev@gmail.com>
2019-03-15 16:30:34 +0200

    - Initial commit bases.

---
+++ app/src/main/java/app/sample/nyt/base/fragment/BaseFragment.java
@@ -0,0 +1,22 @@
+package app.sample.nyt.base.fragment;
+
+import android.support.v4.app.Fragment;
+
+import app.sample.nyt.base.view.MyView;
+
+public class BaseFragment extends Fragment implements MyView {
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+
+    @Override
+    public void handleError() {
+
+    }
+}
---
+++ app/src/main/java/app/sample/nyt/base/view/MyView.java
@@ -0,0 +1,10 @@
+package app.sample.nyt.base.view;
+
+public interface MyView {
+
+    public void showLoading();
+
+    public void hideLoading();
+
+    public void handleError();
+}
